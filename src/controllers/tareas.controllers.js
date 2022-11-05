c8fce6428fada7fbabd123fb12a2d83a0502c59d
nicoelias997 <nicoelias997@gmail.com>
2022-11-05 11:10:32 +0100

    logramos crear y obtener las tareas que creamos mediante ThunderClient

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,7 +1,37 @@
-export const listarTareas = (req, res) => {
-    res.send("Hola, soy GET")
+import { validationResult } from "express-validator"
+import { Tarea } from "../models/tarea"
+
+export const listarTareas = async (req, res) => {
+    try{
+        const tareas = await Tarea.find()
+        res.status(200).json(tareas)
+    } catch(e){
+        res.status(404).json({
+            message: "No encontramos ninguna tarea."
+        })
+    }
+
 }
 
-export const crearTarea = (req, res) => {
-    res.send("Hola, soy POST")
+export const crearTarea = async (req, res) => {
+    try{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                message: "Hay un error, no pudimos crear la tarea."
+            })
+        }
+
+    const nuevaTarea = new Tarea(req.body)
+    await nuevaTarea.save()
+    res.status(201).json({
+        message: "Creamos la tarea con exito"
+    })
+
+    } catch(e){
+        res.status(404).json({
+            message: "Hubo un error, intentelo mas tarde."
+        })
+    }
+
 }
